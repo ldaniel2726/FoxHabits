21b5f64dcf3cf3e0c1ed7552bd49e190dcfddd61
Daniel Lénárt <97975437+ldaniel2726@users.noreply.github.com>
2024-11-25 09:51:19 +0100

    check if user logged in and if it is redirect from signup page to profile

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,8 +1,23 @@
 "use client";
 
 import { SignUpForm } from '@/components/signup-form';
+import { useEffect } from 'react';
+import { supabase } from '../supabaseClient';
+import { useRouter } from 'next/navigation';
 
 export default function LoginPage() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkUser = async () => {
+      const { data } = await supabase.auth.getUser();
+      if (data.user) {
+        router.push('/profile');
+      }
+    };
+    checkUser();
+  }, [router]);
 
   return (
     <div className="flex h-screen w-full items-center justify-center px-4">
