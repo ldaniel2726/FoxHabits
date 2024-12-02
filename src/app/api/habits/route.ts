ee02dbaed8242882a732f334ada52e16bacbfb26
Zente Hollo Cegledi <czh@czh.hu>
2024-12-02 09:26:05 +0100

    Update route.ts

--- src/app/api/habits/route.ts
+++ src/app/api/habits/route.ts
@@ -15,3 +15,79 @@ export async function GET() {
     return NextResponse.json(data);
 }
 
+// Új szokás hozzáadása
+// export async function POST(request: Request) {
+//     const supabase = await createClient();
+//     const { 
+//         habit_name, 
+//         habit_type, 
+//         interval, 
+//         habit_interval_type,
+//         start_date = new Date().toISOString()
+//      } = await request.json();
+  
+//     const {
+//       data: { user },
+//       error: userError,
+//     } = await supabase.auth.getUser();
+  
+//     if (userError || !user) {
+//       return NextResponse.json(
+//         { error: 'A felhasználó nem található! Kérlek jelentkezz be.' },
+//         { status: 401 }
+//       );
+//     }
+  
+//     try {
+
+//       const { data: habitNameData, error: habitNameError } = await supabase
+//         .from('habit_names')
+//         .select('id')
+//         .eq('name', habit_name)
+//         .single(); 
+  
+//       let habitNameId = habitNameData?.id;
+  
+//       if (!habitNameId) {
+//         const { data: newHabitName, error: insertHabitNameError } = await supabase
+//           .from('habit_names')
+//           .insert([{ 
+//             name: habit_name,
+//             habit_name_status: 'new'
+//          }])
+//           .select('id')
+//           .single();
+  
+//         if (insertHabitNameError) {
+//           throw new Error('Hiba a szokásnév rögzítése közben: ' + insertHabitNameError.message);
+//         }
+  
+//         habitNameId = newHabitName.id;
+//       }
+  
+//       const { data: newHabit, error: insertHabitError } = await supabase
+//         .from('habits')
+//         .insert([
+//           {
+//             user_id: user.id,
+//             habit_name_id: habitNameId,
+//             habit_type,
+//             interval,
+//             habit_interval_type,
+//             start_date,
+//           },
+//         ])
+//         .select()
+//         .single();
+  
+//       if (insertHabitError) {
+//         throw new Error('Hiba a szokás rögzítése közben: ' + insertHabitError.message);
+//       }
+  
+//       return NextResponse.json(newHabit, { status: 201 });
+//     } catch (error: unknown) {
+//         const errorMessage = error instanceof Error ? error.message : 'Ismeretlen hiba történt';
+//         return NextResponse.json({ error: errorMessage }, { status: 500 });
+//       }      
+//   }
+  
