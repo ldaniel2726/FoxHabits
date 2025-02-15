ad2668fdb2cd32a9ed05ad8a5a7a48d5fffc4833
Ceglédi Zente Holló <czh@czh.hu>
2025-02-15 20:32:26 +0100

    Add POST /api/checklists

--- src/app/api/checklist/route.ts
+++ src/app/api/checklist/route.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
+import { z } from "zod";
 
+// Saját szokások lekérése
 export async function GET() {
   const supabase = await createClient();
 
@@ -16,8 +18,10 @@ export async function GET() {
     );
   }
 
-  const { data, error } = await supabase.from("checklists").select(
-    `
+  const { data, error } = await supabase
+    .from("checklists")
+    .select(
+      `
         id,
         user_id,
         name,
@@ -25,11 +29,75 @@ export async function GET() {
         created_at,
         updated_at
       `
-  );
+    )
+    .eq("user_id", user.id);
 
   if (error) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 
   return NextResponse.json(data, { status: 200 });
+}
+
+export async function POST(request: Request) {
+  try {
+    const StatusEnum = z.enum(["CHECKED", "UNCHECKED"]);
+
+    const checklistSchema = z.object({
+      id: z.number().positive().int(),
+      user_id: z.string().uuid(),
+      name: z.string().min(1).max(255),
+      elements: z
+        .array(
+          z.object({
+            description: z.string(),
+            status: StatusEnum,
+          })
+        )
+        .default([]),
+    });
+
+    const result = checklistSchema.safeParse(await request.json());
+
+    if (!result.success) {
+      console.log(result.error);
+      return NextResponse.json(
+        { error: result.error.issues[0].message },
+        { status: 400 }
+      );
+    }
+
+    const validatedData = result.data;
+    const supabase = await createClient();
+
+    const {
+      data: { user },
+      error: userError,
+    } = await supabase.auth.getUser();
+
+    if (userError || !user) {
+      return NextResponse.json(
+        { error: "A felhasználó nem található! Kérlek jelentkezz be." },
+        { status: 401 }
+      );
+    }
+
+    const { data, error } = await supabase.from("checklists").insert({
+      user_id: user.id,
+      name: validatedData.name,
+      elements: validatedData.elements,
+    });
+
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+
+    return NextResponse.json(data, { status: 200 });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      { error: `Hibás kérés vagy szerverhiba` },
+      { status: 500 }
+    );
+  }
 }
