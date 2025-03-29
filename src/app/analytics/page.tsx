a4986bc7b9b395b0bef22f443f92bc5f4a9d9f4f
Ceglédi Zente Holló <czh@czh.hu>
2025-03-29 01:12:28 +0100

    Little changes

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -15,7 +15,7 @@ const overallStats = [
 export default function Page() {
   return (
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-10">
-      <h1 className="text-4xl font-bold mb-8">Analitika</h1>
+      <h1 className="text-4xl font-bold mb-8">Statisztika</h1>
       
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mb-8">
         <Card className="lg:col-span-2 transition transform hover:-translate-y-1 hover:shadow-2xl duration-300">
