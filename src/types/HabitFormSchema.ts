8868996de1ae9ddbc66de5d64ef533126b994036
Ceglédi Zente Holló <czh@czh.hu>
2025-04-13 17:03:23 +0200

    Update HabitFormSchema.ts

--- src/types/HabitFormSchema.ts
+++ src/types/HabitFormSchema.ts
@@ -1,12 +1,10 @@
 import { z } from "zod";
 
 export const habitFormSchema = z.object({
-  habit_names: z.string().min(1).max(255),
+  habit_names: z.object({"habit_name": z.string().min(1).max(255)}),
   habit_type: z.enum(["normal_habit", "bad_habit"]),
   interval: z.number().positive(),
   habit_interval_type: z.enum(["hours", "days", "weeks", "months", "years"]),
-  start_date: z.string().date(),
+  start_date: z.string().datetime(),
   is_active: z.boolean().nullable(),
 });
-
-
